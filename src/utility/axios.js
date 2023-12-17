5592aedecf597bf07fe4b7509b9440c463adad41
Freelanceromarr <freelanceromarr@gmail.com>
2023-12-17 07:20:15 +0600

    file updated

--- src/utility/axios.js
+++ src/utility/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://servers.coderbiz.com/',
+    baseURL: 'https://servers.coderbiz.com/',
 })
 export default instance;
